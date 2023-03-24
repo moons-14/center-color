13eec537209a28f6f370a5b8b22a351487dfcfb5
moons14 <moons@moons14.com>
2023-03-25 00:50:19 +0900

    feat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -83,7 +83,7 @@ const CameraDisplay = () => {
   return (
     <div>
       <Head>
-        <title>現実世界の色取得</title>
+        <title>現実世界の色取得 | Center Color</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
